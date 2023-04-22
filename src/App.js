3dfc250e5186faf5879decd3e7a58d4bb691692c
Nayan2305 <thombre23nayan@gmail.com>
2023-04-22 13:11:44 +0530

    Sidebar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 
 import './App.css';
 import {Home} from './pages/index'
-import { ProSidebarProvider } from 'react-pro-sidebar';
 function App() {
   return (
-    <ProSidebarProvider>
-      <Home/>
-    </ProSidebarProvider> 
+    <>
+    <Home/>
+    </>  
     
   );
 }
