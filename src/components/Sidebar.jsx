f9b4bd2dfef9c02d277bebfb597e60adfc652199
Nayan2305 <thombre23nayan@gmail.com>
2023-04-22 17:42:03 +0530

    Add product form

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,11 +2,12 @@
 import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
 import { Link } from 'react-router-dom';
 import './Sidenav.css';
+
 function Slidebar() {
     return(
         
         <div className="sidenav">
-        <a href="#">Orders</a>
+        <a href='../pages/AddProduct.jsx'>Orders</a>
         <br>
         </br>
         <a href="#">Customers</a>
---
+++ src/pages/AddProduct.jsx
@@ -0,0 +1,31 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+function AddProduct() {
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Product Name</Form.Label>
+        <Form.Control type="text" placeholder="Enter product name" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Price</Form.Label>
+        <Form.Control type="number" placeholder="0" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Description</Form.Label>
+        <Form.Control type="text" placeholder="Description" />
+      </Form.Group>
+      
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
+
+export default AddProduct;
