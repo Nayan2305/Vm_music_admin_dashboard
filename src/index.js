07c8b185b6ff5a0255862a3baa745be1e34437b2
Nayan2305 <thombre23nayan@gmail.com>
2023-04-22 09:18:11 +0530

    index.js fixed

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import '../node_modules/font-awesome/css/font-awesome.min.css';
-// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { Provider } from 'react-redux';
-import store from './redux/store';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import {Home} from "./pages";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <Routes>
-        <Route path="/" element={<Home />} />       
-      </Routes>
-    </Provider>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 reportWebVitals();
